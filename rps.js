ff78f1298f239a06b6fa0a4fd534963b02fc34f2
GuranshS <guranshsingh273@gmail.com>
2023-01-13 14:11:59 +0530

    addes css to game

--- rps.js
+++ rps.js
@@ -1,23 +1,25 @@
-const choices = ["rock", "paper", "scissor"];
+const choices = ["Rock", "Paper", "Scissor"];
 let pScore = 0;
 let cScore = 0;
 
 const buttons= document.querySelectorAll("button");
 buttons.forEach(button=>button.addEventListener("click", function(){
-console.log(playRound(this.dataset.value));
+    const board = document.querySelector(".scoreboard");
+    board.innerText = playRound(this.dataset.value);
+    console.log(playRound(this.dataset.value));
     }));
 
 function playRound(PlayerChoice){
     let ComputerChoice = choices[Math.floor(Math.random()*3)];
-    let result = "Your Choice:" + PlayerChoice + "\nOpponent Choice: "+ComputerChoice + "\nYour Score: "
+    let result = "Your Choice: " + PlayerChoice + "\nOpponent Choice: "+ComputerChoice + "\nYour Score: "
     + pScore + "\nComputer Score: "+ cScore;
 
     if (PlayerChoice==ComputerChoice){
         return "Draw\n"+ result;
     }
-    else if ((PlayerChoice=="rock" && ComputerChoice=="scissor")||(PlayerChoice
-        =="scissor" && ComputerChoice=="paper")||(PlayerChoice=="paper" && 
-        ComputerChoice=="rock")){
+    else if ((PlayerChoice=="Rock" && ComputerChoice=="Scissor")||(PlayerChoice
+        =="Scissor" && ComputerChoice=="Paper")||(PlayerChoice=="Paper" && 
+        ComputerChoice=="Rock")){
         pScore++;
         return "You win\n" + result;
     }

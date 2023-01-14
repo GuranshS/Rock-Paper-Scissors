7f07e9d1da799e26805c786f94933a7b81013c3c
GuranshS <guranshsingh273@gmail.com>
2023-01-14 16:11:48 +0530

    added restart functionality

--- rps.js
+++ rps.js
@@ -9,11 +9,38 @@ buttons.forEach(button=>button.addEventListener("click", function(){
     console.log(playRound(this.dataset.value));
     }));
 
+function restart(){
+    const buttons= document.querySelectorAll("button");
+        buttons.forEach(button=>button.addEventListener("click", function(){
+            const board = document.querySelector(".scoreboard");
+            board.innerText = playRound(this.dataset.value);
+            if(!this.dataset.value=="again"){
+                this.disabled = false;
+            }
+        }));
+}
 function playRound(PlayerChoice){
     let ComputerChoice = choices[Math.floor(Math.random()*3)];
     let result = "Your Choice: " + PlayerChoice + "\nOpponent Choice: "+ComputerChoice + "\nYour Score: "
     + pScore + "\nComputer Score: "+ cScore;
+    
+    if (PlayerChoice=="again"){
+        restart();
+        return "You get a second chance";
 
+    }
+    else if (pScore>5 || cScore>5){
+        const buttons= document.querySelectorAll("button");
+        pScore=0;
+        cScore=0;
+        buttons.forEach(button=>button.addEventListener("click", function(){
+        if(!this.dataset.value=="again"){
+            this.disabled = true;
+        }
+        }));
+        return "Press try again to restart"
+    }
+    else { 
     if (PlayerChoice==ComputerChoice){
         return "Draw\n"+ result;
     }
@@ -26,5 +53,6 @@ function playRound(PlayerChoice){
     else{
         cScore++;
         return "You lose\n" + result;
+    }
     }
 }
